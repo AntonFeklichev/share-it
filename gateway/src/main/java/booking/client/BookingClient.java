1aab5f4c45cd38571b2049182a7d361ea2504cdd
Anton <afeklichev@yandex.ru>
2023-10-09 16:33:38 +0300

    Спринт 16 деление на модули 2.0, доделал Booking

---
+++ gateway/src/main/java/booking/client/BookingClient.java
@@ -0,0 +1,67 @@
+package booking.client;
+
+import booking.BookingStatus;
+import booking.CreateBookingDto;
+import client.BaseClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.util.DefaultUriBuilderFactory;
+
+import java.util.Map;
+
+@Component
+public class BookingClient extends BaseClient {
+
+    private static final String API_PREFIX = "/bookings";
+
+    @Autowired
+    public BookingClient(@Value("${my-share-it.sever.URL}") String serverUrl, RestTemplateBuilder builder) {
+        super(
+                builder
+                        .uriTemplateHandler(new DefaultUriBuilderFactory(serverUrl + API_PREFIX))
+                        .requestFactory(() -> new HttpComponentsClientHttpRequestFactory())
+                        .build()
+        );
+    }
+
+    public ResponseEntity<Object> addBooking(CreateBookingDto createBookingDto, Long userId) {
+        return post("", userId, null, createBookingDto);
+    }
+
+
+    public ResponseEntity<Object> approveBooking(Long bookingId, Boolean approved, Long userId) {
+
+        Map<String, Object> parameters = Map.of("/{bookingId}", bookingId,
+                "approved", approved);
+        return patch("/{bookingId}", userId, parameters, null);
+    }
+
+    public ResponseEntity<Object> getBookingStatus(Long bookingId, Long userId) {
+        Map<String, Object> parameters = Map.of("/{bookingId}", bookingId);
+        return get("/{bookingId}", userId, parameters);
+    }
+
+    public ResponseEntity<Object> getBookingList(BookingStatus state, Long userId, Integer from, Integer size) {
+
+        Map<String, Object> parameters = Map.of("state", state.name(),
+                "from", from,
+                "size", size);
+        return get("?state={state}&from={from}&size={size}", userId, parameters);
+
+    }
+
+    public ResponseEntity<Object> getBookingListByOwner(BookingStatus state, Long userId, Integer from, Integer size) {
+
+        Map<String, Object> parameters = Map.of("state", state.name(),
+                "from", from,
+                "size", size);
+
+        return get("?state={state}&from={from}&size={size}", userId, parameters);
+    }
+
+
+}
---
+++ gateway/src/main/java/booking/controller/BookingController.java
@@ -0,0 +1,117 @@
+package booking.controller;
+
+import booking.BookingDto;
+import booking.BookingStatus;
+import booking.CreateBookingDto;
+import booking.client.BookingClient;
+import jakarta.validation.Valid;
+import jakarta.validation.ValidationException;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static constants.Constants.DEFAULT_PAGE_SIZE;
+import static constants.Constants.X_SHARER_USER_ID;
+
+@RestController
+@RequestMapping(name = "/bookings")
+@Validated
+@Slf4j
+public class BookingController {
+
+    private final BookingClient bookingClient;
+
+    @Autowired
+    public BookingController(BookingClient bookingClient) {
+        this.bookingClient = bookingClient;
+    }
+
+
+    public ResponseEntity<Object> addBooking(@Valid
+                                             @RequestBody
+                                             CreateBookingDto createBookingDto,
+                                             @RequestHeader(name = X_SHARER_USER_ID)
+                                             Long userId) {
+        log.info("Add booking by userId {}", userId);
+        return bookingClient.addBooking(createBookingDto, userId);
+    }
+
+
+    @PatchMapping("/{bookingId}")
+    public ResponseEntity<Object> approveBooking(@PathVariable(name = "bookingId")
+                                                 Long bookingId,
+                                                 @RequestParam(name = "approved")
+                                                 Boolean approved,
+                                                 @RequestHeader(name = X_SHARER_USER_ID)
+                                                 Long userId) {
+        log.info("Approve bookingId {} by userId {} ", bookingId, userId);
+        return bookingClient.approveBooking(bookingId, approved, userId);
+    }
+
+    @GetMapping("/{bookingId}")
+    public ResponseEntity<Object> getBookingStatus(@PathVariable(name = "bookingId")
+                                                   Long bookingId,
+                                                   @RequestHeader(name = X_SHARER_USER_ID)
+                                                   Long userId) {
+        log.info("Get booking status by bookingId {}, by userId {}", bookingId, userId);
+        return bookingClient.getBookingStatus(bookingId, userId);
+    }
+
+    @GetMapping
+    public ResponseEntity<Object> getBookingList(@RequestParam(name = "state", defaultValue = "ALL")
+                                                 String stringState,
+                                                 @RequestHeader(name = X_SHARER_USER_ID)
+                                                 Long userId,
+                                                 @RequestParam(name = "from", defaultValue = "0")
+                                                 @PositiveOrZero
+                                                 Integer from,
+                                                 @RequestParam(name = "size",
+                                                         defaultValue = DEFAULT_PAGE_SIZE)
+                                                 @Positive
+                                                 Integer size) {
+        log.info("Get bookingList by parameters: state = {}, from = {}, size = {} by userId",
+                stringState, from, size, userId);
+
+        try {
+            BookingStatus state = BookingStatus.valueOf(stringState);
+            return bookingClient.getBookingList(state, userId, from, size);
+
+        } catch (IllegalArgumentException e) {
+            throw new ValidationException("Unknown state: %s".formatted(stringState));
+
+        }
+
+    }
+
+
+    @GetMapping("/owner")
+    public ResponseEntity<Object> getBookingListByOwner(@RequestParam(defaultValue = "ALL", name = "state")
+                                                  String stateString,
+                                                  @RequestHeader(name = X_SHARER_USER_ID)
+                                                  Long userId,
+                                                  @RequestParam(name = "from",
+                                                          defaultValue = "0")
+                                                  @PositiveOrZero
+                                                  Integer from,
+                                                  @RequestParam(name = "size",
+                                                          defaultValue = DEFAULT_PAGE_SIZE)
+                                                  @Positive
+                                                  Integer size) {
+        log.info("Get bookingList by owner id {} by parameters: state = {}, from = {}, size = {}",
+                userId, stateString, from, size);
+        try {
+            BookingStatus state = BookingStatus.valueOf(stateString);
+            return bookingClient.getBookingListByOwner(state, userId, from, size);
+        } catch (IllegalArgumentException e) {
+            throw new ValidationException("Unknown state: %s".formatted(stateString));
+        }
+    }
+
+
+}
